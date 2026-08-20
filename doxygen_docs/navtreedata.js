6c99fa62d184ad001e92357c62f55270bb022535
lcastela <lcastela@users.noreply.github.com>
2026-08-20 15:55:10 +0000

    deploy: 0ee2fbf1107460a93bea35f0200fb2ee13b6e1a3

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,126 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Eclipse OpenBSW", "index.html#autotoc_md0", null ],
+    [ "README", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html", [
+      [ "Middleware Simulation Example", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md1", [
+        [ "Overview", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md3", null ],
+        [ "Concepts", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md5", null ],
+        [ "Configuration", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md7", null ],
+        [ "Structure", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md9", null ],
+        [ "Initialization", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md11", null ],
+        [ "Services", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md13", [
+          [ "Foo", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md14", null ]
+        ] ],
+        [ "Communication", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md16", [
+          [ "Attribute Broadcasts", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md17", null ],
+          [ "Attribute Get", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md18", null ]
+        ] ],
+        [ "Building", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md20", [
+          [ "Prerequisites", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md21", null ],
+          [ "Process-based execution", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md22", null ],
+          [ "Run", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md23", null ],
+          [ "Regenerating code", "d3/d76/md__home_jenkins_libs_bsw_middleware_simulation_docs_README.html#autotoc_md24", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "README", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html", [
+      [ "C++ Code Generator", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md25", [
+        [ "Prerequisites", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md26", null ],
+        [ "Usage", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md27", [
+          [ "Arguments", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md28", null ]
+        ] ],
+        [ "Quick test with the bundled test deployment", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md29", [
+          [ "Expected output", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md30", null ]
+        ] ],
+        [ "Mock generation mode", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md31", null ],
+        [ "Input structure", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md32", null ],
+        [ "Deployment YAML", "d9/d24/md__home_jenkins_libs_bsw_middleware_tools_cpp_generator_README.html#autotoc_md33", null ]
+      ] ]
+    ] ],
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"",
+"d0/d8a/classasyncNewPlatform_1_1AsyncImplExample.html#a10f3431efb0006abf2f31691b6c5295d",
+"d1/d3b/classudp_1_1UdpIperf2Server.html#ada5b3e32d3b06bccdc6e6dc3dae8de55",
+"d1/dd4/classinterrupts_1_1SuspendResumeAllInterruptsScopedLock.html#a2a835806dbdc415f9479c40e20fff21c",
+"d2/d55/structmiddleware_1_1core_1_1IClusterConnectionConfigurationBidirectional.html",
+"d2/de2/IDiagDispatcher_8h_source.html",
+"d3/d58/classmiddleware_1_1core_1_1ClusterConnectionNoTimeoutSkeletonOnly.html#ab1dde006dde99507618e5147d784b479",
+"d3/dfa/classdoip_1_1IDoIpConnectionHandler.html#aeda65c8e57c9cb564da9d05bbfc8d0b8",
+"d4/d9e/classmiddleware_1_1rpc_1_1ProxyMethodImpl.html#ad5e223f132f1819ece88a51819cb81e6",
+"d5/d59/structbios_1_1BxCanDevice_1_1Config.html#a29aea585bf7b3263976412fb864af08a",
+"d6/d03/structshed_1_1internal_1_1multi__list_1_1NotInBucket.html#a5cad9002e625c35b8b4dae4df46bbf3e",
+"d6/dab/namespaceblob.html#afd2db4449ff56267c25a5602222a24a7",
+"d7/d74/ProxyAttribute_8h.html#a834e244432df9cea5eec9cfe7fe8c71da131fb182a881796e7606ed6da27f1197",
+"d8/d01/classuds_1_1IUdsLifecycleConnector.html#a66f6baad46c1083064428a5146b714fda2f3019f10b1253ac4d37402ac01b3f82",
+"d8/dab/classmiddleware_1_1core_1_1ProxyAttributeBase.html#ac3d5184e4c34c7b25f3391b578ca4cb6",
+"d9/d13/classmiddleware_1_1core_1_1ProxyEventBase.html#ab7d529ea09cc2efad98d9d2665e4976e",
+"d9/dc2/classdoip_1_1DoIpServerVehicleIdentificationConfig.html#aba9cd25992a69f259102ccce5d199bca",
+"da/d8e/structmiddleware_1_1core_1_1Future.html#a42337ac890d4aa9c228ffd1271a0a5d4",
+"da/de9/LockStrategies_8h.html#ac630dee853040b8a6cf466b9b4b19d33",
+"db/d98/structmiddleware_1_1core_1_1IInstanceDatabase.html#aa0f71a669779c96bf84dcbf08294d4a7",
+"dc/d15/classtransport_1_1AbstractTransportLayer.html#a3ef9b63c04601a9f79171d67e82d6961a7188f1f78909d4d5df350eb6ff2a6c46",
+"dc/d7f/structENET__ERXD.html#a6f15826016945b95522663d6a7a9580c",
+"dd/d0e/classuds_1_1IUdsResetConfiguration.html#a8a8e6d594029f1d7a9e18999f954cb53",
+"dd/da6/ITransportMessageProvider_8h_source.html",
+"de/d69/classruntime_1_1FunctionRuntimeStatistics.html#a95f5fa21f0b0e139b5ab7d8bc413068c",
+"df/d21/structshed_1_1internal_1_1type__list__recurse_3_1_1etl_1_1type__list_3_4_01_4.html",
+"df/da9/structdocan_1_1DoCanFrameCodecConfig_1_1SizeConfig.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
